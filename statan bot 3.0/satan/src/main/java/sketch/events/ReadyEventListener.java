bcf4a83daa92cc23c33c2173ee2c7401ed842a64
Paul Anderson <sketchyeye@gmail.com>
2023-04-08 19:02:42 -0400

    added token.class to the gitignore

--- statan bot 3.0/satan/src/main/java/sketch/events/ReadyEventListener.java
+++ statan bot 3.0/satan/src/main/java/sketch/events/ReadyEventListener.java
@@ -6,6 +6,10 @@ import net.dv8tion.jda.api.events.session.ReadyEvent;
 
 public class ReadyEventListener implements EventListener{
     
+    /**
+     * a listener for the ReadyEvent
+     * @param event when the bot is turned on an event is initiated
+     */
     @Override
     public void onEvent(GenericEvent event) {
         if (event instanceof ReadyEvent){
